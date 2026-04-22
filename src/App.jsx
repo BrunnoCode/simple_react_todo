64b200a26bde23483e2566157eba92221ac07729
bbotelho42 <bbotelho@student.42barcelona.com>
2026-04-22 23:45:37 +0200

    created functions onDeleteTaskClick and onTaskClick updating styles and deleting tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import AddTask from './components/AddTask';
 import Tasks from './components/Tasks'
 import { useState } from 'react'
 
@@ -29,11 +28,26 @@ function App() {
   },
 ])
 
+function onTaskClick(taskId){
+  const newTasks = tasks.map(task => {
+    if (task.id === taskId) {
+      return {...task, isCompleted: !task.isCompleted}
+    }
+    return task;
+  })
+  setTasks(newTasks)
+}
+
+function onDeleteTaskClick(taskId){
+  const newTasks = tasks.filter(task => task.id != taskId)
+  setTasks(newTasks)
+}
+
    return (
      <div className='w-screen h-screen bg-purple-300 flex justify-center p-6'>
       <div className='w-[500px]'>
         <h1 className='text-3xl font-bold text-purple-800 text-center font-mono'>Gerenciador de Tarefas</h1>
-        <Tasks tasks={tasks}/>
+        <Tasks tasks={tasks} onTaskClick={onTaskClick} onDeleteTaskClick={onDeleteTaskClick}/>
       
       </div>
      </div>
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,24 +1,27 @@
-import { CircleArrowRight } from "lucide-react";
+import { CircleArrowRight, LucideTrash2 } from "lucide-react";
 
-
-function  Tasks(props){
-  return(
+function Tasks({tasks, onTaskClick, onDeleteTaskClick}) {
+  return (
     <div>
       <ul className="space-y-4 p-6 bg-purple-900 shadow-xl rounded-md">
-        {props.tasks.map((task) =>
+        {tasks.map((task) => (
           <li key={task.id} className="flex gap-2">
-          <button className="text-white text-left bg-purple-700 font-bold rounded-md p-2 w-full">
-            {task.title}
-          </button>
-          <button className="rounded-md text-purple-400">
-            <CircleArrowRight />
-          </button>
+            <button
+              onClick={() => onTaskClick(task.id)}
+              className={`text-white text-left bg-purple-700 font-bold p-2 w-full rounded-md ${task.isCompleted && 'line-through text-purple-200'}`} >
+              {task.title}
+            </button>
+            <button className="text-purple-400">
+              <CircleArrowRight />
+            </button>
+            <button className="text-purple-300" onClick={() =>onDeleteTaskClick(task.id)}>
+              <LucideTrash2 />
+            </button>
           </li>
-        )}
+        ))}
       </ul>
     </div>
-  )
+  );
 }
-
 
 export default Tasks;
