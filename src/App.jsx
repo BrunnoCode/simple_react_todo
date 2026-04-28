601e58ce02f9d367ceb5b8fcceb7f49e824f8158
bbotelho42 <bbotelho@student.42barcelona.com>
2026-04-29 00:31:16 +0200

    Añadidio useEffect de react para persistencia de datos en el local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,57 @@
-import { v4 } from 'uuid'
-import AddTask from './components/AddTask'
-import Tasks from './components/Tasks'
-import { useState } from 'react'
-
-
-
-
+import { v4 } from "uuid";
+import AddTask from "./components/AddTask";
+import Tasks from "./components/Tasks";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tarefas") || []),
+  );
 
-  const [tasks, setTasks] = useState([])
+  useEffect(() => {
+    localStorage.setItem("tarefas", JSON.stringify(tasks));
+  }, [tasks]);
 
-function onTaskClick(taskId){
-  const newTasks = tasks.map(task => {
-    if (task.id === taskId) {
-      return {...task, isCompleted: !task.isCompleted}
-    }
-    return task;
-  })
-  setTasks(newTasks)
-}
+  function onTaskClick(taskId) {
+    const newTasks = tasks.map((task) => {
+      if (task.id === taskId) {
+        return { ...task, isCompleted: !task.isCompleted };
+      }
+      return task;
+    });
+    setTasks(newTasks);
+  }
 
-function onDeleteTaskClick(taskId){
-  const newTasks = tasks.filter(task => task.id != taskId)
-  setTasks(newTasks)
-}
+  function onDeleteTaskClick(taskId) {
+    const newTasks = tasks.filter((task) => task.id != taskId);
+    setTasks(newTasks);
+  }
 
-function onAddTaskSubmit(taskTitle, taskDescription){
-  const newTasks = {
-    id: v4(),
-    title: taskTitle,
-    description: taskDescription,
-    isCompleted: false,
-  } 
-  setTasks([...tasks, newTasks]);
-}
+  function onAddTaskSubmit(taskTitle, taskDescription) {
+    const newTasks = {
+      id: v4(),
+      title: taskTitle,
+      description: taskDescription,
+      isCompleted: false,
+    };
+    setTasks([...tasks, newTasks]);
+  }
 
-   return (
-     <div className='w-screen h-screen bg-purple-300 flex justify-center p-6 '>
-      <div className='w-[500px] space-y-6'>
-        <h1 className='text-3xl font-bold text-purple-800 text-center font-mono'>Gerenciador de Tarefas</h1>
-        <AddTask onAddTaskSubmit={onAddTaskSubmit}/>
-        <Tasks tasks={tasks} onTaskClick={onTaskClick} onDeleteTaskClick={onDeleteTaskClick}/>
+  return (
+    <div className="w-screen h-screen bg-purple-300 flex justify-center p-6 ">
+      <div className="w-[500px] space-y-6">
+        <h1 className="text-3xl font-bold text-purple-800 text-center font-mono">
+          Gerenciador de Tarefas
+        </h1>
+        <AddTask onAddTaskSubmit={onAddTaskSubmit} />
+        <Tasks
+          tasks={tasks}
+          onTaskClick={onTaskClick}
+          onDeleteTaskClick={onDeleteTaskClick}
+        />
       </div>
-     </div>
-   )
+    </div>
+  );
 }
 // AULA PAROU NA HORA 1:31 h
 export default App;
