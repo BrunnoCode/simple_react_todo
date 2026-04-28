37c10c60cb6eda09771d8cc00fcd265bf55b3f87
bbotelho42 <bbotelho@student.42barcelona.com>
2026-04-29 01:31:40 +0200

    ToDo finalizado v0.2

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [tasks, setTasks] = useState(
-    JSON.parse(localStorage.getItem("tarefas") || []),
+    JSON.parse(localStorage.getItem("tarefas") || "[]")
   );
 
   useEffect(() => {
